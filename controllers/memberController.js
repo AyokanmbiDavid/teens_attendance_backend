1b4dea5f88ff09695625253aa4271e4e4ec4f8d6
AyokanmbiDavid <davidayokanmbi47@gmail.com>
2026-04-03 21:27:16 +0100

    teens backend

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -3,15 +3,15 @@ import Member from '../models/Member.js';
 export const addMember = async (req, res) => {
     try {
         // This line prevents the "Cannot destructure" crash
-        const { title, phone_number, date_of_birth } = req.body || {};
+        const { surname,firstName,middleName,phoneNumber, dateOfBirth,gender,emailAddress } = req.body || {};
 
-        if (!title || !phone_number) {
+        if (!surname || !firstName || !middleName || !phoneNumber) {
             return res.status(400).json({ 
                 message: "Body is empty or missing title/phone. Check your Postman settings!" 
             });
         }
 
-        const newMember = new Member({ title, phone_number, date_of_birth });
+        const newMember = new Member({ surname,firstName,middleName,phoneNumber,dateOfBirth,gender,emailAddress});
         await newMember.save();
         res.status(201).json(newMember);
     } catch (err) {
@@ -31,7 +31,7 @@ export const getAllMembers = async (req, res) => {
 // Add this to your existing exports
 export const updateMember = async (req, res) => {
     try {
-        const { id } = req.params;
+        const {id} = req.params;
         const updatedMember = await Member.findByIdAndUpdate(id, req.body, { new: true });
         if (!updatedMember) return res.status(404).json({ message: "Member not found" });
         res.json(updatedMember);
--- models/Member.js
+++ models/Member.js
@@ -4,10 +4,10 @@ const memberSchema = new mongoose.Schema({
   surname: {type: String, required: true},
   firstName : { type: String, required: true },
   middleName : {type: String, required: true},
-  gender: {type: String,enum:['Male','Female'], required: true},
+  gender: {type: String, required: true},
   phoneNumber: { type: String, trim: true, required: true},
-  dateOfBirth: { type: String },
-  emailAddress: {type: String, trim:true, lowercase: true,},
+  dateOfBirth: { type: String, required: true},
+  emailAddress: {type: String, trim:true, lowercase: true, required:true},
 }, { timestamps: true });
 
 // CHANGE THIS LINE:
