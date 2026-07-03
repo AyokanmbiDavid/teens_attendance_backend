fa05673c1ac94c0c05c053d8831712566d2bb788
Ayokanmbi David <davidayokanmbi47@gmail.com>
2026-07-03 23:24:20 +0100

    mc backend

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(cors({
   origin: [
     'https://miracle-center-teenager-attendance.vercel.app', // Removed trailing slash
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'https://miracle-center-children-attendance.vercel.app/'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'] 
